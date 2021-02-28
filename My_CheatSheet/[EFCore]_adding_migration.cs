9fd9c25771bf7cdc82d504e1b8f3e94f108ad21a
Szymon Oles <szyoles@gmail.com>
2021-02-28 20:57:42 +0100

    new files

--- My_CheatSheet/[EFCore]_adding_migration.cs
+++ My_CheatSheet/[EFCore]_adding_migration.cs
@@ -1,3 +1,4 @@
 //nuget MS.EFCore.Tools requires
 //to add migration in cmd type:
 add-migration <name>
+update-database
--- My_CheatSheet/[EFCore]_editing_migration.cs
+++ My_CheatSheet/[EFCore]_editing_migration.cs
@@ -1,1 +1,4 @@
 //we can edit migration using migrationBuilder.Sql(<yourSqlQuery>);
+
+//renaming column might be equal with loss of data (removing column => adding new column)
+
